99fb0f770241965f5967493da84d79eae92aa7f3
Emily <edd@64.125.179.128.available.above.net>
2013-04-20 15:46:45 -0400

    fixed crashing, what does it do?

--- js/toastymap.js
+++ js/toastymap.js
@@ -1,6 +1,6 @@
 $(function(){
 	$.get("/data/GOS_vs_pfam-trunc.txt", function(data){
-		var object = $.tsv.toArrays(data`);
+		var object = $.tsv.toArrays(data, {stripHeader: true});
 		console.log("Object generated");
 		var clustered = clusterfck.hcluster(object);
 		console.log(clustered);
