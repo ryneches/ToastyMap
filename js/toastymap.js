576b74d2737fa78b0cfc5dfb335d725e7fd7c28f
Emily <edd@64.125.179.128.available.above.net>
2013-04-20 16:29:44 -0400

    Added cleaned truncated data

--- js/toastymap.js
+++ js/toastymap.js
@@ -1,5 +1,5 @@
 $(function(){
-	$.get("/data/GOS_vs_pfam-trunc.txt", function(data){
+	$.get("/data/GOS_vs_pfam-trunc-clean.txt", function(data){
 		var object = $.tsv.toArrays(data, {stripHeader: true});
 		console.log("Object generated");
 		var clustered = clusterfck.hcluster(object);
